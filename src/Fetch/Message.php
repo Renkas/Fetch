99d9bdcd2e0d15f2700df5aabf3ff1002fbca2ef
Rene Saare <renkas@gmail.com>
2020-02-20 13:26:24 +0200

    Update Message.php

--- src/Fetch/Message.php
+++ src/Fetch/Message.php
@@ -545,7 +545,8 @@ class Message
                 }
             }
 
-            if (strtolower($structure->subtype) === 'plain' || ($structure->type == 1 && strtolower($structure->subtype) !== 'alternative')) {
+            if (strtolower($structure->subtype) === 'plain' || ($structure->type == 1 && strtolower($structure->subtype) !== 'alternative'&& strtolower($structure->subtype) !== 'related'
+)) {
                 if (isset($this->plaintextMessage)) {
                     $this->plaintextMessage .= PHP_EOL . PHP_EOL;
                 } else {
